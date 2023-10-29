eff44df266fe8a69eae70321553a5b3d47857c86
Yourwalkerex <r_k_walker@yahoo.com>
2023-10-28 18:22:34 -0700

    Set up base game file structure
    Defaults added to the nothing.

---
+++ autoloads/globals.gd
@@ -0,0 +1,26 @@
+class_name GLOBAL extends Node
+
+### COLORS ###
+
+const ORANGE_ONE = Color(0.8196, 0.6941, 0.5294, 1)
+const ORANGE_TWO = Color(0.7804, 0.4824, 0.3451, 1)
+const ORANGE_THREE = Color(0.6824, 0.3647, 0.251, 1)
+const ORANGE_FOUR = Color(0.4745, 0.2667, 0.2902, 1)
+const ORANGE_FIVE = Color(0.2941, 0.2392, 0.2667, 1)
+
+const BROWN_ONE = Color(0.7294, 0.5686, 0.3451, 1)
+const BROWN_TWO = Color(0.5725, 0.4549, 0.2549, 1)
+const BROWN_THREE = Color(0.302, 0.2706, 0.2235, 1)
+
+const GREEN_ONE = Color(0.702, 0.6471, 0.3333, 1)
+const GREEN_TWO = Color(0.4667, 0.4549, 0.2314, 1)
+
+const BLUE_ONE = Color(0.549, 0.6706, 0.6314, 1)
+const BLUE_TWO = Color(0.2941, 0.4471, 0.4314, 1)
+
+const GRAY_ONE = Color(0.8235, 0.7882, 0.6471, 1)
+const GRAY_TWO = Color(0.6706, 0.6078, 0.5569, 1)
+const GRAY_THREE = Color(0.5176, 0.4706, 0.4588, 1)
+const GRAY_FOUR = Color(0.3412, 0.2824, 0.3216, 1)
+
+const BLACK = Color(0.1255, 0.1255, 0.1255, 1)
---
+++ autoloads/signal_bus.gd
@@ -0,0 +1,12 @@
+class_name SignalBus extends Node
+
+
+signal initialize_game
+
+
+signal movement_locked
+signal movement_unlocked
+
+
+signal dialog_emitted(emitter)
+
